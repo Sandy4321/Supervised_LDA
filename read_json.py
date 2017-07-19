97eaa122360dcf5a5ab66c690042762b7c1772fe
Jaspreet Sandhu <jaspreetsan@gmail.com>
2017-07-20 01:14:07 +0200

    updated read_json script for random sampling

--- read_json.py
+++ read_json.py
@@ -1,36 +1,42 @@
 import sys
 import json
 import os
+from random import random
 import pandas as pd
 import numpy as np
 
-#Take dataset folder path as argument
-dir_path = sys.argv[1]
-print dir_path
+def convert_json_to_csv(path_to_json_files, number_of_lines=10000, output_file='test_new.py'):
 
-content = []
+    content = []
 
-for fil in os.listdir(dir_path):
-    fil_path = os.path.join(dir_path, fil)
-    print fil_path
-    label = fil_path.split('_')[1]
-    i=0
+    for fil in os.listdir(path_to_json_files):
+        fil_path = os.path.join(path_to_json_files, fil)
+        print fil_path
 
-    with open(fil_path, 'r') as f:
-        for i,line in enumerate(f):
-            if i < 10000:
-                pass
-            elif (i > 10000) and (i < 15000):
-                l = json.loads(line)
-                content.append([l['reviewText'], label])
-            # print json.dumps(l, indent=6, sort_keys=True)
-            elif i>15000:
-                break
+        label = fil_path.split('_')[1]
+        n=0
 
-# content_arr = np.array(content)
-content_df = pd.DataFrame(content, columns=['Text', 'Label'])
-print content_df.head()
-print content_df['Label'].value_counts()
+        with open(fil_path, 'r') as f:
 
-f_out = "test.csv"
-content_df.to_csv(f_out)
+            for line in f:
+
+                if (random() < 0.1) and (n < int(number_of_lines)/2):
+                    l = json.loads(line)
+                    content.append([l['reviewText'], label])
+                    n += 1
+
+                elif (n >= int(number_of_lines)/2):
+                    break
+
+                else:
+                    pass
+
+    # content_arr = np.array(content)
+    content_df = pd.DataFrame(content, columns=['Text', 'Label'])
+    print content_df.head()
+    print content_df['Label'].value_counts()
+
+    content_df.to_csv(output_file)
+
+
+convert_json_to_csv(sys.argv[1], sys.argv[2], sys.argv[3])
